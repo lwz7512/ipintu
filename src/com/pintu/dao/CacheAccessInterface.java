fe62d8ad548d5f36cb104eefebb45dc5dc529db5
liumingli <liu3385ml@163.com>
2011-07-25 17:06:37 +0800

    test

--- src/com/pintu/dao/CacheAccessInterface.java
+++ src/com/pintu/dao/CacheAccessInterface.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.pintu.beans.Comment;
 import com.pintu.beans.Story;
